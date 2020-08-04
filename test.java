76cdf393d8c24f030bdf4ac012c728efbaaf9c5a
dor-co <dor713cohen@gmail.com>
2020-08-04 19:26:57 +0300

    third

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ package jenkins_with_git;
 
 public class test {
 	public static void main(String[] args) {
-		int num1 = 2;
+		int num1 = 3;
 		int num2 = 3;
 		
 		System.out.println("The sum of num1 and num2 is: " + (num1+num2));
